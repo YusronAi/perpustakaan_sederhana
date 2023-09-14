7981f1e76a97b473a7813694feefb2b00a026a8b
YusronAi <yus777999777@gmail.com>
2023-09-14 20:07:04 +0700

    Membuat sebuah project perpustakaan sederhana dengan beberapa file dan folder terlebih dahulu

---
+++ index.php
@@ -0,0 +1,17 @@
+<! DOCTYPE html>
+<html>
+  <head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title></title>
+  <?php include "page/head.php"; ?>
+  </head>
+  <body>
+    <?php include "page/navbar.php"; ?>
+    <div class="container-fluid"> 
+       buatkan banner
+    </div>
+    <!-- <?php include "page/footer.php" ?> -->
+    <?php include "page/script.php" ?>
+  </body>
+</html>
